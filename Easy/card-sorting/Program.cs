52948e25ed2f02dc7f75ac3df51b9f8831b6600b
Branbados <brandonaltermatt@gmail.com>
2021-07-10 20:17:06 -0700

    updated gitignore for C#, initialized card-sorting

---
+++ Easy/card-sorting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace card_sorting
+{
+    class Card{
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
