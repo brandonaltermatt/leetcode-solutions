a98c582d14bf12e3e1726f48604b506ddc3230f5
Brandon Altermatt <brandonaltermatt@gmail.com>
2021-08-17 17:28:22 -0700

    added count_largest_group

---
+++ Easy/count_largest_group.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var countLargestGroup = function(n) {
+    let biggest_sum = 0;
+    let sum_of_digits = 0;
+
+    for(let i=0; i<n; i++){
+        let number_string = i.toString();
+        
+        for (let j=0; j < number_string.length; j++){
+            sum_of_digits += parseInt(number_string[j])
+            console.log(`${i}\t${parseInt(number_string[j])}`)
+        }
+        
+        if (sum_of_digits > biggest_sum){
+            biggest_sum = sum_of_digits;
+        }
+
+        sum_of_digits = 0;
+    }
+
+    return biggest_sum;
+};
+
+countLargestGroup(13)
