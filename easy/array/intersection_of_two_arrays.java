20a0682f61d7d193704782353683f70904be82c4
Brandon Altermatt <brandonaltermatt@gmail.com>
2021-09-21 18:10:25 -0700

    added another java solution to intersection of two arrays

--- easy/array/intersection_of_two_arrays.java
+++ easy/array/intersection_of_two_arrays.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Map;
-
 /*
 https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/674/
 Given two integer arrays nums1 and nums2, return an array of their intersection. 
@@ -8,6 +5,39 @@ Each element in the result must appear as many times as it shows in both arrays
 and you may return the result in any order.
 */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+class Solution{
+    public int[] intersect(int[] nums1, int[] nums2){
+        int[] intersection = new int[nums1.length];
+        int i=0, j=0, k=0;
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        while((i < nums1.length) && (j < nums2.length)){
+            int nums1_current = nums1[i];
+            int nums2_current = nums2[j];
+
+            if(nums1_current < nums2_current){
+                i++;
+            } else if(nums1_current > nums2_current){
+                j++;
+            } else {
+                intersection[k] = nums1_current;
+                i++;
+                j++;
+                k++;
+            }
+        }
+
+        return Arrays.copyOfRange(intersection, 0, k);
+    }
+}
+
+/* Solution for unsorted arrays with a hash map and an arraylist
 class Solution{
     public int[] intersect(int[] nums1, int[] nums2){
         Map<Integer, Integer> frequencies = new HashMap<>();
@@ -31,3 +61,4 @@ class Solution{
         return result;
     }
 }
+*/
