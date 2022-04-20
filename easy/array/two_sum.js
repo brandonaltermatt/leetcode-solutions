b0725ea50a237628901c2a3bffe0b3125b6f4ea6
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-04-20 14:39:28 -0700

    Added two sum solutions

---
+++ easy/array/two_sum.js
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/problems/two-sum/
+ * Given an array of integers nums and an integer target, 
+ * return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, 
+ * and you may not use the same element twice.
+ * You can return the answer in any order.
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i += 1) {
+    let currentValue = nums[i];
+    let complementPair = target - currentValue;
+
+    if (map[complementPair] !== undefined) {
+        return [map[complementPair], i];
+    } else {
+      map[currentValue] = i;
+    }
+  }
+};
+
+/* Solution that iterates twice with indexOf
+ var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i += 1) {
+    const secondIndex = nums.indexOf(target - nums[i]);
+    if ((secondIndex !== -1) && (secondIndex !== i)) return [i, secondIndex];
+  }
+};
+*/
