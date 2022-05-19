4683ab620c8b048c4b8611c0b2458f63af2711ce
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-05-19 10:29:12 -0700

    Added solutions to find distinct step counts climbing stairs

--- easy/dynamic-programming/climbing_stairs.js
+++ easy/dynamic-programming/climbing_stairs.js
@@ -1,19 +1,35 @@
-/**
- * https://leetcode.com/explore/interview/card/top-interview-questions-easy/97/dynamic-programming/569/
- * You are climbing a staircase. It takes n steps to reach the top.
- * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-**/
+/* https://leetcode.com/explore/interview/card/top-interview-questions-easy/97/dynamic-programming/569/
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
 
 /**
  * @param {number} n
  * @return {number}
  */
- var climbStairs = function(n) {
-    let step_count = [];
-    step_count [1]=1;
-    step_count [2]=2;
-    for(let i=3; i<=n; i++){
-        step_count[i] = step_count[i-1] + step_count[i-2];
-    }
-    return step_count[n];
+const climbStairs = function (n) {
+  if (n < 3) return n;
+
+  let secondStepCount = 1;
+  let firstStepCount = 1;
+
+  for (let i = 2; i <= n; i += 1) {
+    const currentStepCount = secondStepCount + firstStepCount;
+    secondStepCount = firstStepCount;
+    firstStepCount = currentStepCount;
+  }
+
+  return firstStepCount;
 };
+
+/* Solution using dynamic programming
+const climbStairs = function (n) {
+  const stepPaths = [0, 1, 2];
+
+  for (let i = 3; i <= n; i += 1) {
+    stepPaths[i] = stepPaths[i - 1] + stepPaths[i - 2];
+  }
+
+  return stepPaths[n];
+};
+*/
