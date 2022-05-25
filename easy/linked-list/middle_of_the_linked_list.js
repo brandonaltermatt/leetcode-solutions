ea13bede1d21661a8aad44f2af9222db8551e100
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-05-25 14:22:39 -0700

    Added solution for finding the middle node of a linked list

---
+++ easy/linked-list/middle_of_the_linked_list.js
@@ -0,0 +1,26 @@
+/* https://leetcode.com/problems/middle-of-the-linked-list/
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  return slow;
+};
