bd178f8f05326da02f1e8ed91208dc1c6be6d6b4
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-05-31 16:03:52 -0700

    Added problem description and link

--- easy/binary/number_of_1_bits.js
+++ easy/binary/number_of_1_bits.js
@@ -1,3 +1,8 @@
+/* https://leetcode.com/problems/number-of-1-bits/
+Write a function that takes an unsigned integer and returns
+the number of '1' bits it has (also known as the Hamming weight).
+*/
+
 /**
  * @param {number} n - a positive integer
  * @return {number}
