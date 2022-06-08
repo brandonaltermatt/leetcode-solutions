35655fd537ca2dd48a9487b1c8efb0987e116279
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-06-08 13:11:29 -0700

    Added iterative solution to identifying symmetric trees.

--- easy/trees/symmetric_tree.js
+++ easy/trees/symmetric_tree.js
@@ -18,6 +18,63 @@ it is a mirror of itself (i.e., symmetric around its center).
 const isSymmetric = function (root) {
   if (!root) return true;
 
+  const isMirror = (p, q) => {
+    const stackP = [p];
+    const stackQ = [q];
+
+    while (stackP.length || stackQ.length) {
+      const currentP = stackP.pop();
+      const currentQ = stackQ.pop();
+
+      if (!currentP && !currentQ) continue;
+
+      if ((!currentP || !currentQ) || (currentP.val !== currentQ.val)) return false;
+
+      stackP.push(currentP.left);
+      stackP.push(currentP.right);
+      stackQ.push(currentQ.right);
+      stackQ.push(currentQ.left);
+    }
+
+    return true;
+  };
+
+  return isMirror(root.left, root.right);
+};
+
+/* Using queues, BFS
+const isSymmetric = function (root) {
+  if (!root) return true;
+
+  const isMirror = (p, q) => {
+    const queueP = [p];
+    const queueQ = [q];
+
+    while (queueP.length || queueQ.length) {
+      const currentP = queueP.shift();
+      const currentQ = queueQ.shift();
+
+      if (!currentP && !currentQ) continue;
+
+      if ((!currentP || !currentQ) || (currentP.val !== currentQ.val)) return false;
+
+      queueP.push(currentP.left);
+      queueP.push(currentP.right);
+      queueQ.push(currentQ.right);
+      queueQ.push(currentQ.left);
+    }
+
+    return true;
+  };
+
+  return isMirror(root.left, root.right);
+};
+*/
+
+/* Solution using recursion
+const isSymmetric = function (root) {
+  if (!root) return true;
+
   const isMirror = (s, t) => {
     if (!s && !t) return true;
 
@@ -28,3 +85,4 @@ const isSymmetric = function (root) {
 
   return isMirror(root.left, root.right);
 };
+*/
