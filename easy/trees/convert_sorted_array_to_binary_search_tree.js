cdaf1d88c4bef701e9e8eb2f944f94f5463fe119
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-06-13 16:46:13 -0700

    Added solution to convert sorted array into a balanced BST.

---
+++ easy/trees/convert_sorted_array_to_binary_search_tree.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+/* https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+Given an integer array nums where the elements are sorted in ascending order,
+convert it to a height-balanced binary search tree.
+A height-balanced binary tree is a binary tree in which the depth of the two
+subtrees of every node never differs by more than one.
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+const sortedArrayToBST = function (nums) {
+  if (!nums.length) return null;
+
+  const mid = Math.floor(nums.length / 2);
+  const root = new TreeNode(nums[mid]);
+
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+
+  return root;
+};
