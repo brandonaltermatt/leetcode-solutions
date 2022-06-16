5fe9e7b8373049144b2655ea55469b7688a12a38
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-06-16 10:10:21 -0700

    Added iterative solution to test if tree contains subtree

--- easy/trees/subtree_of_another_tree.js
+++ easy/trees/subtree_of_another_tree.js
@@ -21,6 +21,33 @@ The tree tree could also be considered as a subtree of itself.
  */
 const isSubtree = function (root, subRoot) {
   const isSameTree = (root1, root2) => {
+    const stack = [[root1, root2]];
+
+    while (stack.length) {
+      const [node1, node2] = stack.pop();
+      if (!node1 && !node2) continue;
+      if ((!node1 || !node2) || (node1.val !== node2.val)) return false;
+      stack.push([node1.left, node2.left], [node1.right, node2.right]);
+    }
+
+    return true;
+  };
+
+  const stack = [root];
+
+  while (stack.length) {
+    const node = stack.pop();
+    if (!node) continue;
+    if (isSameTree(node, subRoot)) return true;
+    stack.push(node.right, node.left);
+  }
+
+  return false;
+};
+
+/* Recursive solution
+const isSubtree = function (root, subRoot) {
+  const isSameTree = (root1, root2) => {
     if (!root1 || !root2) return !root1 && !root2;
     if (root1.val !== root2.val) return false;
     return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
@@ -31,3 +58,4 @@ const isSubtree = function (root, subRoot) {
   || isSubtree(root.left, subRoot)
   || isSubtree(root.right, subRoot);
 };
+*/
