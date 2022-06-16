186b1768c3402fc819135bbad472ade7bae074d2
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-06-16 10:29:42 -0700

    Added non-sorting solution to finding squares of a sorted array.

--- easy/array/squares_of_a_sorted_array.js
+++ easy/array/squares_of_a_sorted_array.js
@@ -7,5 +7,20 @@ return an array of the squares of each number sorted in non-decreasing order.
  * @return {number[]}
  */
 const sortedSquares = function (nums) {
+  const result = [];
+  let left = 0;
+  let right = nums.length - 1;
+  let i = right;
+
+  while (left <= right) {
+    result[i--] = ((nums[left] ** 2) > (nums[right] ** 2)) ? nums[left++] ** 2 : nums[right--] ** 2;
+  }
+
+  return result;
+};
+
+/* Solution using sort function.
+const sortedSquares = function (nums) {
   return nums.map((element) => element ** 2).sort((a, b) => a - b);
 };
+*/
