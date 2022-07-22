ab646c03aed3f467b3b31bef44dd1704de1e8e63
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-07-22 10:57:46 -0700

    Added problem description

--- medium/stack/evaluate_reverse_polish_notation.js
+++ medium/stack/evaluate_reverse_polish_notation.js
@@ -1,3 +1,12 @@
+/* https://leetcode.com/problems/evaluate-reverse-polish-notation/submissions/
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+Note that division between two integers should truncate toward zero.
+It is guaranteed that the given RPN expression is always valid.
+That means the expression would always evaluate to a result,
+and there will not be any division by zero operation.
+*/
+
 /**
  * @param {string[]} tokens
  * @return {number}
