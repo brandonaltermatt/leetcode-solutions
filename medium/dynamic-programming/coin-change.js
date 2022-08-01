26afe7d5f7f694a070b66f96c06c5a14334a810e
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-08-01 15:55:53 -0700

    Added memoization solution to coin change problem

--- medium/dynamic-programming/coin-change.js
+++ medium/dynamic-programming/coin-change.js
@@ -11,6 +11,30 @@ You may assume that you have an infinite number of each kind of coin.
  * @param {number} amount
  * @return {number}
  */
+// Recursion using memoization to cache overlapping subproblems
+const coinChangeMemoization = function (coins, amount) {
+  const minChange = (denominations, remainder, memo) => {
+    if (remainder === 0) return 0;
+    if (remainder < 0) return -1;
+    if (memo[remainder]) return memo[remainder];
+
+    let minCount = Infinity;
+
+    for (let i = 0; i < denominations.length; i++) {
+      const currentCount = minChange(denominations, remainder - denominations[i], memo);
+      if ((currentCount >= 0) && (currentCount < minCount)) {
+        minCount = 1 + currentCount;
+      }
+    }
+
+    memo[remainder] = (minCount === Infinity) ? -1 : minCount;
+
+    return memo[remainder];
+  };
+
+  return minChange(coins, amount, []);
+};
+
 // Inefficient recursive solution, recomputes overlapping subproblems
 const coinChangeRecursion = function (coins, amount) {
   if (amount === 0) return 0;
