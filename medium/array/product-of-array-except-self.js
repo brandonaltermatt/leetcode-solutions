32d27f5b06b9440ef2172d96abf83b5b150489f2
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-08-02 13:39:23 -0700

    Solved finding product of array values except self, in three passes using prefix and suffix arrays

--- medium/array/product-of-array-except-self.js
+++ medium/array/product-of-array-except-self.js
@@ -9,6 +9,29 @@ You must write an algorithm that runs in O(n) time and without using the divisio
  * @param {number[]} nums
  * @return {number[]}
  */
+// Solution using 3 loops, O(3N) == O(N)
+const productExceptSelfThreeLoops = function (nums) {
+  const prefixes = new Array(nums.length);
+  const suffixes = new Array(nums.length);
+  const result = new Array(nums.length);
+
+  prefixes[0] = 1;
+  for (let i = 1; i < nums.length; i++) {
+    prefixes[i] = prefixes[i - 1] * nums[i - 1];
+  }
+
+  suffixes[nums.length - 1] = 1;
+  for (let i = nums.length - 2; i >= 0; i--) {
+    suffixes[i] = suffixes[i + 1] * nums[i + 1];
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefixes[i] * suffixes[i];
+  }
+
+  return result;
+};
+
 // Solution using division, O(4N) == O(N)
 const productExceptSelfWithDivision = function (nums) {
   const zeroCount = nums.filter((num) => (num === 0)).length;
