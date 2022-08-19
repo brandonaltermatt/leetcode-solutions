79678c5ca0ce53c872bb6b6dc3a0fa907a6ffd7a
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-08-19 13:25:09 -0700

    Added comments and clearer names for merging email accounts solution

--- medium/graph/accounts-merge.js
+++ medium/graph/accounts-merge.js
@@ -21,34 +21,40 @@ const accountsMerge = function (accounts) {
   const emailOwner = {};
   const parents = {};
 
-  const find = (x) => {
-    if (parents[x] !== x) parents[x] = find(parents[x]);
+  /** Travel up the list of parents for an email until we find the root parent */
+  const findRootParent = (email) => {
+    if (parents[email] !== email) parents[email] = findRootParent(parents[email]);
 
-    return parents[x];
+    return parents[email];
   };
 
-  const union = (x, y) => {
-    parents[find(x)] = find(y);
+  /** Union the groups by assigning the root parent of email 2
+   * to be a child of the root parent of email 1 */
+  const unionGroup = (email1, email2) => {
+    parents[findRootParent(email1)] = findRootParent(email2);
   };
 
   accounts.forEach(([name, ...emails]) => {
     emails.forEach((email) => {
+      // Set a parent for the email if none exists
       if (!parents[email]) parents[email] = email;
 
       emailOwner[email] = name;
-      union(email, emails[0]);
+      // Group emails from the same account together by assigning the same root parent
+      unionGroup(email, emails[0]);
     });
   });
 
-  const emails = {};
-  Object.keys(parents).forEach((email) => {
-    const parent = find(email);
-    if (emails[parent]) {
-      emails[parent].push(email);
+  const emailGroups = {};
+  Object.keys(parents).forEach((parent) => {
+    const rootParent = findRootParent(parent);
+    if (emailGroups[rootParent]) {
+      emailGroups[rootParent].push(parent);
     } else {
-      emails[parent] = [email];
+      emailGroups[rootParent] = [parent];
     }
   });
 
-  return Object.entries(emails).map(([email, x]) => [emailOwner[email], ...x.sort()]);
+  return Object.entries(emailGroups)
+    .map(([parent, children]) => [emailOwner[parent], ...children.sort()]);
 };
