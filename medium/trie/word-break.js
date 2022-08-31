29db01cdbe9196d1f66b18b28aedd6a6634d8e5d
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-08-31 13:15:51 -0700

    Added optimized solution for word break.

--- medium/trie/word-break.js
+++ medium/trie/word-break.js
@@ -9,14 +9,14 @@ Note that the same word in the dictionary may be reused multiple times in the se
  * @param {string[]} words
  * @return {boolean}
  */
-const wordBreak = function (s, words) {
+const wordBreak1 = function (s, words) {
   // Use a set for O(1) lookups for words
   const wordSet = new Set(words);
   // Use an array representing indexes of the string to mark whether a word
   // from the word set can start at an index of the string
-  const canWordStartAtIndex = new Array(s.length + 1).fill(false);
+  const starts = new Array(s.length + 1).fill(false);
   // Assume the first character of the string is the beginning of a word from the word set
-  canWordStartAtIndex[0] = true;
+  starts[0] = true;
 
   // Go through the string, testing every index to see if a word can end at that index.
   // If a word can end at that index, mark it as true, and use that index as a starting point
@@ -24,17 +24,37 @@ const wordBreak = function (s, words) {
   for (let end = 1; end <= s.length; end++) {
     for (let start = 0; start < end; start++) {
       // Skip iteration if the index isn't a valid start of a word
-      if (canWordStartAtIndex[start] === false) continue;
+      if (starts[start] === false) continue;
 
       // Skip iteration if word set does not contain the word sliced from the string
       const word = s.slice(start, end);
       if (!wordSet.has(word)) continue;
 
       // Mark that a word from the word set was found, and ends at this index
-      canWordStartAtIndex[end] = true;
+      starts[end] = true;
       break;
     }
   }
 
-  return canWordStartAtIndex[s.length];
+  return starts[s.length];
+};
+
+const wordBreak2 = function (s, wordArray) {
+  const words = new Set(wordArray);
+  const wordLengths = new Set(wordArray.map((word) => word.length));
+  // Use a set to save the indexes of the string that can be used as the start of words.
+  const starts = new Set([0]);
+
+  // For every index that can be the start of a word, check whether any substring
+  // with the length of a word from the word set is a valid word.
+  // If so, add the end index of the substring to the list of start indexes.
+  starts.forEach((start) => {
+    wordLengths.forEach((length) => {
+      if (words.has(s.slice(start, start + length))) {
+        starts.add(start + length);
+      }
+    });
+  });
+
+  return starts.has(s.length);
 };
