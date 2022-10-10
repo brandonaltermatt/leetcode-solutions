c2b7bd4c7698339aaddf6614f9bfc7cf81caddd4
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-10-10 09:27:12 -0700

    Added solution for finding nodes visible from right side of binary tree using breadth first search

--- medium/binary-tree/binary-tree-right-side-view.js
+++ medium/binary-tree/binary-tree-right-side-view.js
@@ -15,6 +15,27 @@ return the values of the nodes you can see ordered from top to bottom.
  * @param {TreeNode} root
  * @return {number[]}
  */
+const rightSideViewBfs = function (root) {
+  if (root === null) return [];
+
+  const result = [];
+  const queue = [root];
+
+  while (queue.length !== 0) {
+    const size = queue.length;
+
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+
+      if (i === size - 1) result.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+
+  return result;
+};
+
 const rightSideViewDfs = function (root) {
   const result = [];
 
