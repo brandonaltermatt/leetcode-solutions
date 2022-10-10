085a6f4e59c3f999e4adefab34a79a92b55129a4
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-10-10 11:13:53 -0700

    Added solution for finding if an array can be partitioned into two equal subsets using a knapsack

--- medium/array/partition-equal-subset-sum.js
+++ medium/array/partition-equal-subset-sum.js
@@ -10,6 +10,36 @@ Note: There are many solutions for this problem. See here:
  * @param {number[]} nums
  * @return {boolean}
  */
+// This can be a knapsack problem, where we want to find a combination that sums to sum(nums)/2.
+// We'll create a matrix to represent our possible knapsack choices.
+// We want to find a combination C[i][w] where the first i items sums to w.
+const canPartitionKnapsack = function (nums) {
+  const numsSum = nums.reduce((acc, curr) => acc + curr);
+  if ((numsSum % 2) !== 0) return false;
+
+  // Create a row with space for each num, plus 0
+  let matrix = new Array(nums.length + 1).fill(null);
+  // Create columns with space for sums up to half the sum of nums
+  matrix = matrix.map((column) => Array((numsSum / 2) + 1).fill(false));
+
+  // Represents whether we can get sum = 0 with 0 elements
+  matrix[0][0] = true;
+
+  for (let i = 1; i <= nums.length; i++) {
+    const currentNum = nums[i - 1];
+
+    for (let j = 0; j <= numsSum / 2; j++) {
+      if (j - currentNum >= 0) {
+        matrix[i][j] = matrix[i - 1][currentNum] || matrix[i - 1][j];
+      } else {
+        matrix[i][j] = matrix[i - 1][j];
+      }
+    }
+  }
+
+  return matrix[nums.length][numsSum / 2];
+};
+
 // Checks every combination of sums to find if one equals half the sum of nums, O(2^n)
 const canPartitionHashSet = function (nums) {
   const totalSum = nums.reduce((acc, curr) => acc + curr);
