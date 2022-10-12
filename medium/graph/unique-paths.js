abffaf5faae53bf103f72840e9970a4652ea9d4a
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-10-12 15:21:26 -0700

    Added solution to find all unique paths from top left to bottom right on grid using brute force recursion

---
+++ medium/graph/unique-paths.js
@@ -0,0 +1,28 @@
+/* https://leetcode.com/problems/unique-paths/
+There is a robot on an m x n grid. The robot is initially located
+at the top-left corner (i.e., grid[0][0]).The robot tries to move
+to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot
+can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths
+that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+*/
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+// Try every possible path, O(2^(m+n))
+const uniquePathsBruteForce = function (m, n, i = 0, j = 0) {
+  const dfs = (row, col) => {
+    if ((row === (m - 1)) && (col === (n - 1))) return 1;
+    if ((row > (m - 1)) || (col > (n - 1))) return 0;
+
+    return dfs(row + 1, col) + dfs(row, col + 1);
+  };
+
+  return dfs(0, 0);
+};
