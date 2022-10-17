771faa976e228658a6fd762c4a0bba8eacfd51ae
Brandon Altermatt <brandonaltermatt@gmail.com>
2022-10-17 09:25:42 -0700

    Added solution for finding all anagrams in a string using sliding window method

--- medium/string/find-all-anagrams-in-a-string.js
+++ medium/string/find-all-anagrams-in-a-string.js
@@ -11,6 +11,34 @@ typically using all the original letters exactly once.
  * @param {string} p
  * @return {number[]}
  */
+// Use a sliding window with a hash map to find anagrams. O(2N) => O(N)
+const findAnagramsSlidingWindow = function (s, p) {
+  const hashP = new Array(26).fill(0);
+
+  // For every p[i], increment map[p[i]] to indicate which letters are anagram candidates
+  for (let i = 0; i < p.length; i++) {
+    hashP[p.charCodeAt(i) - 97]++;
+  }
+
+  const anagramStarts = [];
+  let windowStart = 0;
+
+  // For every s[i], decrement map[s[i]] to indicate which letters have been seen
+  for (let i = 0; i < s.length; i++) {
+    const char1 = s.charCodeAt(i) - 97;
+    hashP[char1]--;
+    // If s[i] is a letter not in p, move the window start to i, resetting the map along the way
+    while (hashP[char1] < 0) {
+      const char2 = s.charCodeAt(windowStart) - 97;
+      windowStart++;
+      hashP[char2]++;
+    }
+    if ((i - windowStart) + 1 === p.length) anagramStarts.push(windowStart);
+  }
+
+  return anagramStarts;
+};
+
 // Check all substrings of length p in s to find anagrams. O(N*M)
 const findAnagramsBruteForce = function (s, p) {
   const isAnagram = (stringA, stringB) => {
